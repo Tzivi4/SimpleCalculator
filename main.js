190d0a971d4c83794eaf6b32cace89b56280c88c
Tzivi <stavgamer00@gmail.com>
2020-08-05 10:53:47 -0300

    Fist commit

---
+++ main.js
@@ -0,0 +1,146 @@
+var cifra = "";
+var acumulation = 0;
+var sumar = false;
+var restar = false;
+var restarpr = true;
+var mul = false;
+var mulpr = true
+var div = false;
+
+function displayNumeros(numero) {
+
+    document.getElementById("txt").value = cifra + numero;
+
+    cifra = document.getElementById("txt").value;
+
+}
+
+function displaySuma() {
+    if (restar) {
+        acumulation = acumulation - parseInt(cifra);
+        document.getElementById("txt").value = acumulation;
+    } else if (mul) {
+        acumulation = acumulation * parseInt(cifra);
+        document.getElementById("txt").value = acumulation;
+    } else if (div) {
+        acumulation = acumulation / parseInt(cifra);
+        document.getElementById("txt").value = acumulation;
+    } else {
+        acumulation = acumulation + parseInt(cifra);
+        document.getElementById("txt").value = acumulation;
+    }
+    cifra = "";
+    sumar = true;
+    restar = false;
+    mul = false;
+    div = false;
+    restarpr = false;
+
+}
+
+function displayResta() {
+
+    if (restarpr == false) {
+        if (sumar) {
+            acumulation = acumulation + parseInt(cifra);
+            document.getElementById("txt").value = acumulation;
+        }
+        else if (mul) {
+            acumulation = acumulation * parseInt(cifra);
+            document.getElementById("txt").value = acumulation;
+        } else if (div) {
+            acumulation = acumulation / parseInt(cifra);
+            document.getElementById("txt").value = acumulation;
+        } else {
+            acumulation = acumulation - parseInt(cifra);
+            document.getElementById("txt").value = acumulation;
+        }
+    } else {
+        acumulation = parseInt(cifra);
+        restarpr = false;
+    }
+    cifra = "";
+    sumar = false;
+    restar = true;
+    mul = false;
+    div = false;
+
+}
+
+function displayMul() {
+        if (restar) {
+            acumulation = acumulation - parseInt(cifra);
+            document.getElementById("txt").value = acumulation;
+        }
+        else if (sumar) {
+            acumulation = acumulation + parseInt(cifra);
+            document.getElementById("txt").value = acumulation;
+        } else if (div) {
+            acumulation = acumulation / parseInt(cifra);
+            document.getElementById("txt").value = acumulation;
+        } else {
+            acumulation = acumulation * parseInt(cifra);
+            document.getElementById("txt").value = acumulation;
+        }
+    cifra = "";
+    mul = true;
+    sumar = false;
+    restar = false;
+    div = false;
+}
+
+function displayDiv() {
+
+    if (restar) {
+        acumulation = acumulation - parseInt(cifra);
+        document.getElementById("txt").value = acumulation;
+    }
+    else if (mul) {
+        acumulation = acumulation * parseInt(cifra);
+        document.getElementById("txt").value = acumulation;
+    } else if (sumar) {
+        acumulation = acumulation + parseInt(cifra);
+        document.getElementById("txt").value = acumulation;
+    } else {
+        acumulation = acumulation / parseInt(cifra);
+        document.getElementById("txt").value = acumulation;
+    }
+
+    cifra = "";
+    mul = false;
+    sumar = false;
+    restar = false;
+    div = true;
+
+}
+
+function displayEraseNum() {
+
+}
+
+function displayResult() {
+
+    if (sumar) {
+        document.getElementById("txt").value = acumulation + parseInt(cifra);
+    } else if (restar) {
+        document.getElementById("txt").value = acumulation - parseInt(cifra);
+    } else if (mul) {
+        document.getElementById("txt").value = acumulation * parseInt(cifra);
+    } else if (div) {
+        document.getElementById("txt").value = acumulation / parseInt(cifra);
+    }
+    acumulation = parseInt(document.getElementById("txt").value);
+    cifra = 0;
+}
+
+function displayErase() {
+
+    document.getElementById("txt").value = null;
+    cifra = "";
+    acumulation = 0;
+
+}
+
+function share() {
+    prompt("Copy this link and Share: ", "file:///C:/Users/Tzivi/Desktop/Calculadora%20en%20internet/index.html#");
+}
